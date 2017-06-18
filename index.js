6ff1788ea7ae13e349d50820a22b823c03e45bad
enriquedevs <nitos_san@hotmail.com>
2017-06-18 17:37:37 -0500

    [gh-followers] Getting github users on table
    Getting github users on table from public github api

--- index.js
+++ index.js
@@ -2,6 +2,18 @@
 
 const app = angular.module('followersApp', []);
 
-app.controller('MainController', function($scope){
+app.controller('MainController', function($scope, $http){
+	$scope.githubUsers = [];
 	
+	$scope.init = function(){
+		$scope.getGithubUsers();
+	}
+	
+	$scope.getGithubUsers = function(){
+		$http.get('https://api.github.com/users?since=0').then(function(response) {
+			$scope.githubUsers = response.data;
+		});
+	}
+	
+	$scope.init();
 });
